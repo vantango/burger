b74f810ac79f4f984dd5a25915a20bd4a6bd59bb
Aleks Kimball-Moreno <alekskimball@gmail.com>
2021-01-25 20:32:32 -0800

    got devour and burger ready buttons functional

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Bacon King', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burg', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesy Melt', true);
